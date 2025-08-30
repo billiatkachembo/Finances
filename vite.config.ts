8e8e1cb251d65d1e8cfe3f2acfe5894d4c0e9571
Billiat Kachembo <billiatkachembo1@gmail.com>
2025-08-31 00:43:18 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  headers: {
+    // Correctly allow unsafe-eval for trusted scripts
+    'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'eval' https://apis.google.com https://www.gstatic.com;"
+  },
   plugins: [
     react(),
   ],
@@ -18,17 +22,16 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
+    // Disable sourcemaps to hide eval() warning from gapi-script
     sourcemap: false,
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Put React and ReactDOM in their own chunk
             if (id.includes('react-dom') || id.includes('react')) {
               return 'react';
             }
-            // Put other libraries in the vendor chunk
             return 'vendor';
           }
         },
